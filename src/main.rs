53c4abe4983ffdbabee1386925a276ecf8bc9f7a
Memo Chou <memochou1993@gmail.com>
2022-11-17 23:09:36 +0800

    Update main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::time::Duration;
+
+use chrono::Local;
+
+#[rustfmt::skip]
+const DIGITS : [[&str; 11]; 7] = [
+    ["┏━┓ ","  ╻  "," ┏━┓ ", " ┏━┓ "," ╻ ╻ "," ┏━┓ "," ┏   "," ┏━┓ "," ┏━┓ "," ┏━┓ ","   "],
+    ["┃ ┃ ","  ┃  ","   ┃ ", "   ┃ "," ┃ ┃ "," ┃   "," ┃   ","   ┃ "," ┃ ┃ "," ┃ ┃ "," ╻ "],
+    ["┃ ┃ ","  ┃  ","   ┃ ", "   ┃ "," ┃ ┃ "," ┃   "," ┃   ","   ┃ "," ┃ ┃ "," ┃ ┃ ","   "],
+    ["┃ ┃ ","  ┃  "," ┏━┛ ", " ┣━┫ "," ┗━┫ "," ┗━┓ "," ┣━┓ ","   ┃ "," ┣━┫ "," ┗━┫ ","   "],
+    ["┃ ┃ ","  ┃  "," ┃   ", "   ┃ ","   ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ┃ ┃ ","   ┃ ","   "],
+    ["┃ ┃ ","  ┃  "," ┃   ", "   ┃ ","   ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ╹ "],
+    ["┗━┛ ","  ╹  "," ┗━━ ", " ┗━┛ ","   ╹ "," ┗━┛ "," ┗━┛ ","   ╹ "," ┗━┛ "," ┗━┛ ","   "],
+];
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        std::process::Command::new("clear").status().unwrap();
+        let time = Local::now().format("%H:%M:%S").to_string();
+        for row in &DIGITS {
+            for c in time.chars() {
+                let col = match c {
+                    '0'..='9' => c as usize - '0' as usize,
+                    ':' => 10,
+                    _ => 10,
+                };
+                print!("{} ", row[col]);
+            }
+            println!();
+        }
+        std::thread::sleep(Duration::from_millis(1000))
+    }
 }
